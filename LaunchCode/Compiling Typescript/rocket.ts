72346f13c1cca5d4298245e8a46718b08a67ce47
Austin King <austintriesemail@gmail.com>
2020-12-17 18:40:04 -0600

    created initial file

---
+++ LaunchCode/Compiling Typescript/rocket.ts
@@ -0,0 +1,58 @@
+import { Astronaut } from "./astronaut";
+import { Cargo } from "./cargo";
+import { Payload } from "./Payload";
+
+
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
+
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+
+    sumMass(items:Payload[]): number{
+
+        let sumMass:number = 0;
+
+        for(let item of items){
+            sumMass += item.massKg
+
+        }
+
+        return sumMass;
+    }
+
+    currentMassKg(): number{
+        let sumOfAstronaut: number = this.sumMass(this.astronauts);
+        let sumOfCargoItems: number = this.sumMass(this.cargoItems);
+
+        return sumOfAstronaut + sumOfCargoItems;
+    }
+
+    canAdd(item: Payload): boolean{
+
+        return this.currentMassKg() + item.massKg <= this.totalCapacityKg
+    }
+    addCargo(cargo: Cargo): boolean{
+        if(this.canAdd(cargo)){
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    addAstronaut(astronaut: Astronaut): boolean{
+        if(this.canAdd(astronaut)){
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
